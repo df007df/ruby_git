d454dfdd4e13491963575b7070def3c9ab1a2e54
df007df <duanfeisj@gmail.com>
2013-08-19 20:11:14 +0800

    Update user.sql
    WITH GRANT OPTION

--- config/user.sql
+++ config/user.sql
@@ -1,4 +1,4 @@
 CREATE USER '{{dbuser}}'@'{{host}}' IDENTIFIED BY '{{password}}';
 GRANT USAGE ON * . * TO '{{dbuser}}'@'{{host}}' IDENTIFIED BY '{{password}}' WITH MAX_QUERIES_PER_HOUR 0 MAX_CONNECTIONS_PER_HOUR 0 MAX_UPDATES_PER_HOUR 0 MAX_USER_CONNECTIONS 0 ;
 CREATE DATABASE IF NOT EXISTS \`{{dbname}}\`;
-GRANT ALL PRIVILEGES ON \`{{dbname}}\` . * TO '{{dbuser}}'@'{{host}}';
+GRANT ALL PRIVILEGES ON \`{{dbname}}\` . * TO '{{dbuser}}'@'{{host}}' WITH GRANT OPTION;
